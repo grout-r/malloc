8edae44c236b3da83a30cc2a1f7510ebf28d2027
Cedric Voinnet <cedric.voinnet@epitech.eu>
2015-01-30 16:38:41 +0100

    mem.h 0.1

---
+++ mem.h
@@ -0,0 +1,17 @@
+/*
+** mem.h for mem in /home/voinne_c/malloc/mallocu
+** 
+** Made by Cédric Voinnet
+** Login   <voinne_c@epitech.net>
+** 
+** Started on  Fri Jan 30 13:37:35 2015 Cédric Voinnet
+** Last update Fri Jan 30 13:50:33 2015 Cédric Voinnet
+*/
+
+#ifndef MEM_H_
+# define MEM_H_
+
+extern void	*start;
+unsigned int    get_size(void *ptr);
+
+#endif	/* !MEM_H_ */
