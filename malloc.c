87ff84b16664d407d10c5136e3b7d280d6e38ac0
Cedric Voinnet <cedric.voinnet@epitech.eu>
2015-02-07 10:58:29 +0100

    Split 1.0

--- malloc.c
+++ malloc.c
@@ -5,7 +5,7 @@
 ** Login   <voinne_c@epitech.net>
 ** 
 ** Started on  Tue Feb  3 09:59:09 2015 Cédric Voinnet
-** Last update Sat Feb  7 09:52:57 2015 Cédric Voinnet
+** Last update Sat Feb  7 10:54:12 2015 Cédric Voinnet
 */
 
 #include <unistd.h>
@@ -29,9 +29,9 @@ void		*split(void *ptr, size_t size)
       
       new = ((char*)ptr + META_SIZE + size);
       
-      new_size = (old_size + META_SIZE) - (size + META_SIZE) - META_SIZE;
+      new_size = old_size - (DATA_SIZE + size) - META_SIZE;
 
-      printf("old_size : %lu -- size : %lu  -- new size : %lu\n", old_size + META_SIZE , size + META_SIZE , new_size + META_SIZE);
+      printf("old_size : %lu -- size : %lu  -- new size : %lu\n", old_size, size, new_size);
 
       *(char*)(new) = 0;
       *(size_t*)((char*)new + DATA_FREE) = new_size;
@@ -45,8 +45,8 @@ void	*find_space(void *ptr, size_t size)
   while (ptr != sbrk(0))
     {
       if (*(char*)ptr == 0 && *(size_t*)(ptr + DATA_FREE) >= size)
-	return ptr;
-	//	return (split(ptr, size));
+	//return ptr;
+	return (split(ptr, size));
       ptr = ptr + *(size_t*)(ptr + DATA_FREE) + META_SIZE;
     }
   return (NULL);
